786038f0d2e961b12717895d9ea771a66f82af37
Sheldon Codling <shelcod@gmail.com>
2026-03-28 00:46:16 -0400

    Added basic logic for moving around an arena

--- Sketch.ino
+++ Sketch.ino
@@ -1,10 +1,12 @@
 #include "player.h"
+#include "level.h"
 #include <Arduboy2.h>
 #include <ArduboyTones.h>
 
 Arduboy2 ab;
 ArduboyTones sound(ab.audio.enabled);
 Player player(&ab, &sound);
+Level level(&ab, 0b11111111);
 
 void setup() {
   ab.begin();
@@ -24,9 +26,10 @@ void loop() {
 }
 
 void update() {
-
+  player.update();
 }
 
 void draw() {
   player.draw();
+  level.draw(player.get_map_scroll_x(), player.get_map_scroll_y());
 }
