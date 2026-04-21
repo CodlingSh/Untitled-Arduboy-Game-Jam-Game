27bd5e23884b5dbedc7b4342d05a554c22e78fa9
Sheldon Codling <shelcod@gmail.com>
2026-04-20 22:57:00 -0400

    started moving the clouds and deciding what direction they should move in

--- cloudMap.h
+++ cloudMap.h
@@ -16,9 +16,9 @@ const uint8_t PROGMEM cloudMask[] = {
 // Cloud maps
 const uint16_t map1[16] = {
   0b0000000000000000,
-  0b0000000000000000,
+  0b0001000000000000,
   0b0011100000000000,
-  0b0000000000000000,
+  0b0001000000000000,
   // 0b1110101010101111,
   // 0b0101010101010101,
   // 0b1010101010101010,
@@ -41,10 +41,42 @@ class CloudMap {
   private:
     float xOffset = 0;
     float yOffset = 0;
+    float speed = 0.25;
+    bool windBlowing = false;
+    uint8_t windDirection = 5; // 0 = N, 1 = E, 2 = S, 3 = W, 5 = none
+    uint8_t secondTimer = 60;
+    uint8_t windTimer = 0;
   public:
 
     void update() {
       // yOffset -= 0.25;
+      if (windBlowing == false) {
+        secondTimer--;
+
+        if (secondTimer == 0) {
+          windBlowing = true;
+          secondTimer = 60;
+        }
+      }
+
+      if (windBlowing == true && windDirection == 5) {
+        windDirection = random(4);
+      }
+        
+
+      switch(windDirection) {
+        case 0: // North
+          yOffset += speed;
+          break;
+        case 1: // East
+          xOffset += speed;
+          break;
+        case 2: // South
+          yOffset -= speed;
+          break;
+        case 3: // West
+          xOffset -= speed;
+      }
     }
 
     void draw() {
